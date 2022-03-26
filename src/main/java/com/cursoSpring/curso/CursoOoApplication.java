8ea72ca947ea62ee99638e30d3b86e35942dda7f
Thays Fernandes <thaysfernandes.ti@gmail.com>
2022-03-26 09:06:48 -0300

    primeirocommit

---
+++ src/main/java/com/cursoSpring/curso/CursoOoApplication.java
@@ -0,0 +1,13 @@
+package com.cursoSpring.curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoOoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoOoApplication.class, args);
+	}
+
+}
