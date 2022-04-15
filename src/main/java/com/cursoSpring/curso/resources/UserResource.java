612992cdb4e2ac7af2dd6453e70f4b8acc627e17
Thays Fernandes <thaysfernandes.ti@gmail.com>
2022-04-14 22:18:42 -0300

    Criacao de repositories e config

--- src/main/java/com/cursoSpring/curso/resources/UserResource.java
+++ src/main/java/com/cursoSpring/curso/resources/UserResource.java
@@ -1,17 +1,18 @@
 package com.cursoSpring.curso.resources;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cursoSpring.curso.entities.User;
 
-@RestController
+
 @RequestMapping(value = "/users")
+@RestController
 public class UserResource {
 	
-	@GetMapping
+	@RequestMapping(method=RequestMethod.GET)
 	public ResponseEntity<User> findAll() {
 		
 		User u = new User(1L, "Thays", "thaysfernandes.ti@gmail.com", "81162992", "81162992");
